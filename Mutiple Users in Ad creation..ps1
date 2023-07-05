25fa26059ac3893832dd9363d532df91b76c40bd
gopala.krishnan <valid.gopalakrishnan.2390@gmail.com>
2023-07-05 07:35:41 +0530

    adding mutiple user

---
+++ Mutiple Users in Ad creation..ps1
@@ -0,0 +1,21 @@
+﻿#Creating mutilple users 
+
+$users = Import-Csv "C:\R\t.csv"
+
+
+ foreach ($user in $users) {
+$use = ($user.first+"."+$user.lastname)
+$displayname = $user.first + $user.lastname
+$d = '@sapiens.com'
+$email = $use+$d
+$first = $user.first
+$lastname = $user.lastname
+$password = $user.password
+
+ 
+New-ADUser -SamAccountName $use -UserPrincipalName $email  -name $use  -DisplayName $displayname -Path "OU=Service&Guest_Accounts,OU=IT-stuff,DC=sapiens,DC=int" -GivenName $first -Surname $lastname -EmailAddress $email -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force  ) -ChangePasswordAtLogon $true -Enabled $true
+
+
+}
+
+
