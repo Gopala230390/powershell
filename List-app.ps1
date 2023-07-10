3a7b17dccf53b5586c9988c40459aba61ef1c5ad
gopala.krishnan <valid.gopalakrishnan.2390@gmail.com>
2023-07-10 19:59:14 +0530

    list apps in windows

---
+++ List-app.ps1
@@ -0,0 +1,1 @@
+﻿Get-WmiObject -Class Win32_Product | Select-Object IdentifyingNumber , Name | Format-Table | export-csv -Path "C:\intel\appse.csv"
--- Mutiple Users in Ad creation..ps1
+++ Mutiple Users in Ad creation..ps1
@@ -1,21 +1,16 @@
-﻿#Creating mutilple users 
-
-$users = Import-Csv "C:\R\t.csv"
+﻿
+$users = Import-Csv 'C:\R\y .csv'
 
 
  foreach ($user in $users) {
 $use = ($user.first+"."+$user.lastname)
-$displayname = $user.first + $user.lastname
 $d = '@sapiens.com'
 $email = $use+$d
 $first = $user.first
 $lastname = $user.lastname
 $password = $user.password
 
- 
-New-ADUser -SamAccountName $use -UserPrincipalName $email  -name $use  -DisplayName $displayname -Path "OU=Service&Guest_Accounts,OU=IT-stuff,DC=sapiens,DC=int" -GivenName $first -Surname $lastname -EmailAddress $email -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force  ) -ChangePasswordAtLogon $true -Enabled $true
 
 
 }
-
 
